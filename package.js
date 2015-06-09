1469191308a782388a8d947a7ee94e045b43dcf3
Ben Greene <ben@benmgreene.com>
2015-06-09 09:43:33 -0400

    0.7.2

--- package.js
+++ package.js
@@ -1,22 +1,14 @@
 Package.describe({
   name: 'benmgreene:dagre',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '0.7.2',
+  summary: 'Directed graph renderer for javascript',
+  git: 'https://github.com/benmgreene/meteor-dagre',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
-  api.addFiles('dagre.js');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('benmgreene:dagre');
-  api.addFiles('dagre-tests.js');
+  api.versionsFrom('METEOR@1.0');
+  api.export('dagre', 'client');
+  api.addFiles('dagre/dist/dagre.js', 'client');
+  api.addFiles('export.js', 'client');
 });
